05d41ce7c08db077088bebc553ac877f10c1d6b2
Filipe Esch <filipe.esch@farfetch.com>
2020-03-24 15:37:37 +0000

    feat: adds next middleware invocation after typed handler execution

--- src/KafkaFlow.TypedHandler/TypedHandlerMiddleware.cs
+++ src/KafkaFlow.TypedHandler/TypedHandlerMiddleware.cs
@@ -38,6 +38,8 @@ namespace KafkaFlow.TypedHandler
                         context.Message)
                     .ConfigureAwait(false);
             }
+
+            await next();
         }
     }
 }
