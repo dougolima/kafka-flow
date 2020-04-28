7195c2025f25eba6d2f29bd4f0307482e27bb8c5
FARFETCH\douglas.lima <douglas.lima@farfetch.com>
2020-04-28 14:22:54 +0100

    test: Refactoring code style in unit tests

--- src/KafkaFlow.UnitTests/OffsetManagerTests.cs
+++ src/KafkaFlow.UnitTests/OffsetManagerTests.cs
@@ -13,13 +13,13 @@ namespace KafkaFlow.UnitTests
     {
         private Mock<IConsumer<byte[], byte[]>> consumerMock;
         private TopicPartition topicPartition;
-        private OffsetManager offsetManager;
+        private OffsetManager target;
 
         public OffsetManagerTests()
         {
             this.consumerMock = new Mock<IConsumer<byte[], byte[]>>();
             this.topicPartition = new TopicPartition("topic-A", new Partition(1));
-            this.offsetManager = new OffsetManager(
+            this.target = new OffsetManager(
                 consumerMock.Object, 
                 new List<TopicPartition> { topicPartition });
         }
@@ -28,7 +28,7 @@ namespace KafkaFlow.UnitTests
         public void StoreOffset_WithoutInitialization_ThrowsException()
         {
             // Act
-            Action act = () => offsetManager.StoreOffset(new TopicPartitionOffset(topicPartition, new Offset(1) ));
+            Action act = () => this.target.StoreOffset(new TopicPartitionOffset(this.topicPartition, new Offset(1) ));
             
             // Assert
             act.Should().Throw<InvalidOperationException>();
@@ -38,10 +38,10 @@ namespace KafkaFlow.UnitTests
         public void StoreOffset_WithInvalidTopicPartition_ShouldDoNothing()
         {
             // Arrange
-            offsetManager.InitializeOffsetIfNeeded(new TopicPartitionOffset(topicPartition, new Offset(1)));
+            this.target.InitializeOffsetIfNeeded(new TopicPartitionOffset(this.topicPartition, new Offset(1)));
             
             // Act
-            offsetManager.StoreOffset(new TopicPartitionOffset(new TopicPartition("topic-B", new Partition(1)), new Offset(1)));
+            this.target.StoreOffset(new TopicPartitionOffset(new TopicPartition("topic-B", new Partition(1)), new Offset(1)));
             
             // Assert
             this.consumerMock.Verify(c => c.StoreOffset(It.IsAny<TopicPartitionOffset>()), Times.Never());
@@ -51,17 +51,17 @@ namespace KafkaFlow.UnitTests
         public void StoreOffset_WithGaps_ShouldStoreOffsetJustOnce()
         {
             // Arrange
-            offsetManager.InitializeOffsetIfNeeded(new TopicPartitionOffset(topicPartition, new Offset(1)));
+            this.target.InitializeOffsetIfNeeded(new TopicPartitionOffset(this.topicPartition, new Offset(1)));
             
             // Act
-            offsetManager.StoreOffset(new TopicPartitionOffset(topicPartition, new Offset(3)));
-            offsetManager.StoreOffset(new TopicPartitionOffset(topicPartition, new Offset(2)));
-            offsetManager.StoreOffset(new TopicPartitionOffset(topicPartition, new Offset(1)));
+            this.target.StoreOffset(new TopicPartitionOffset(this.topicPartition, new Offset(3)));
+            this.target.StoreOffset(new TopicPartitionOffset(this.topicPartition, new Offset(2)));
+            this.target.StoreOffset(new TopicPartitionOffset(this.topicPartition, new Offset(1)));
             
             // Assert
             this.consumerMock.Verify(c => 
                 c.StoreOffset(It.Is<TopicPartitionOffset>(p => 
-                    p.Partition.Equals(topicPartition.Partition) &&
+                    p.Partition.Equals(this.topicPartition.Partition) &&
                     p.Offset.Value.Equals(4))), 
                 Times.Once);
         }
